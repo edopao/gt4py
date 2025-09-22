1c557a616492b8f8c3416f3892a164f38720e99f
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-09-22 17:37:35 +0200

    Added nvtx instrumention

--- src/gt4py/next/program_processors/runners/dace/workflow/translation.py
+++ src/gt4py/next/program_processors/runners/dace/workflow/translation.py
@@ -265,7 +265,17 @@ class DaCeTranslator(
             #   top-level SDFG with a cpp timer (std::chrono). This timer measures
             #   only the computation time, it does not include the overhead of
             #   calling the SDFG from Python.
-            sdfg.instrument = dace.dtypes.InstrumentationType.Timer
+            collect_gpu_tx_markers = config.COLLECT_METRICS_LEVEL == metrics.GPU_TX_MARKERS and _has_gpu_schedule(sdfg)
+            sdfg.instrument = dace.dtypes.InstrumentationType.GPU_TX_MARKERS if collect_gpu_tx_markers else dace.dtypes.InstrumentationType.Timer
+            if collect_gpu_tx_markers:
+                for node in sdfg.all_nodes_recursive():
+                    if isinstance(node, dace.sdfg.SDFGState):
+                        node.instrument = dace.dtypes.InstrumentationType.GPU_TX_MARKERS
+                    elif isinstance(node, dace.sdfg.SDFG):
+                        node.instrument = dace.dtypes.InstrumentationType.GPU_TX_MARKERS
+                    elif isinstance(node, dace.nodes.EntryNode):
+                        node.instrument = dace.dtypes.InstrumentationType.GPU_TX_MARKERS
+
         elif self.async_sdfg_call:
             async_sdfg_call = True
 
